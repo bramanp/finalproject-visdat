06fd8a28cc60e4e893f341cf83431f71a60be935
bramanp <71612936+bramanp@users.noreply.github.com>
2022-06-16 17:52:19 +0700

    Update mytubesapp.py

--- mytubesapp.py
+++ mytubesapp.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[25]:
-
-
 import pandas as pd
 from bokeh.io import curdoc
 from bokeh.plotting import figure, show
@@ -14,16 +11,9 @@ from bokeh.layouts import widgetbox, row, gridplot, column
 from bokeh.models import Slider, Select
 
 
-# In[26]:
-
-
-# baca dataset
+# baca dataset, sumber:(https://www.kaggle.com/datasets/hendratno/covid19-indonesia)
 data = pd.read_csv("covid_19_indonesia_time_series_all.csv")
 data.head()
-
-
-# In[27]:
-
 
 # memastikan kolom "Date" bertipe "datetime64"
 data['Date'] = pd.to_datetime(data['Date'])  
@@ -32,24 +22,8 @@ data['Date'] = pd.to_datetime(data['Date'])
 data.set_index('Date', inplace=True)
 data.head()
 
-
-# In[28]:
-
-
 # membuat list unique dari kolom 'Location'
 location_list = pd.unique(data['Location']).tolist()
-location_list
-
-
-# In[29]:
-
-
-# Make a color mapper: color_mapper
-color_mapper = CategoricalColorMapper(factors=location_list, palette=Spectral11)
-
-
-# In[30]:
-
 
 # membuat ColumnDataSource
 source = ColumnDataSource(data={
@@ -57,24 +31,12 @@ source = ColumnDataSource(data={
     'y' : data["New Cases"],
 })
 
-
-# In[31]:
-
-
 # membuat figure plot
 plot = figure(title='New Cases COVID-19', x_axis_label='Date', y_axis_label='New Cases',
            x_axis_type='datetime' ,plot_height=600, plot_width=1200)
 
-
-# In[32]:
-
-
 # membuat graph
 plot.circle(x='x', y='y', source=source, fill_alpha=0.8)
-
-
-# In[33]:
-
 
 # fungsi update plot
 def update_plot(attr, old, new):
@@ -97,10 +59,6 @@ def update_plot(attr, old, new):
         }
     source.data = new_data
 
-
-# In[34]:
-
-
 # dropdown untuk y axis
 # Kasus
 y_select = Select(
@@ -120,10 +78,6 @@ y_kota_select = Select(
 )
 
 y_kota_select.on_change('value', update_plot)
-
-
-# In[35]:
-
 
 layout = row(column(y_kota_select, y_select), plot)
 curdoc().add_root(layout)
